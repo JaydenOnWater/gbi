378e84458a795d7d72ef4021a72f6d83d12df675
JaydenOnWater <88438766+JaydenOnWater@users.noreply.github.com>
2023-05-03 22:29:39 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to GamerBoyInd Texture Pack!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Made bY <code>YouTube/@JaydenOnFire</code>
         </p>
       </main>
 
